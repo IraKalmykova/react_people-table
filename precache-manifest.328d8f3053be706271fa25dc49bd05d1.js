4d55e89b6e7319116c345c7a5547acc47c236c38
Ira <kalmykova_ira@ukr.net>
2019-07-15 10:42:51 +0300

    Updates

---
+++ precache-manifest.328d8f3053be706271fa25dc49bd05d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1050477f868bef2931c0ce15943a7ed1",
+    "url": "/react_people-table/index.html"
+  },
+  {
+    "revision": "1e6caaeef18c514db4c3",
+    "url": "/react_people-table/static/css/main.f6956556.chunk.css"
+  },
+  {
+    "revision": "7fec1989c766e6bde66f",
+    "url": "/react_people-table/static/js/2.429d6ea0.chunk.js"
+  },
+  {
+    "revision": "1e6caaeef18c514db4c3",
+    "url": "/react_people-table/static/js/main.b9ad3f9e.chunk.js"
+  },
+  {
+    "revision": "c6b2e506936d419d6d52",
+    "url": "/react_people-table/static/js/runtime~main.b2b73302.js"
+  }
+]);
